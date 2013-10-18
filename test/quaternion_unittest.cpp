11df42aac4c600e679fff29e79e2b1dad6e1b04f
Jacob McGladdery <jacobm117@gmail.com>
2013-10-18 19:45:29 -0400

    Began adding unit tests

--- test/quaternion_unittest.cpp
+++ test/quaternion_unittest.cpp
@@ -23,3 +23,103 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include "../utility/quaternion.h"
 #include <gtest/gtest.h>
+#include <cmath>
+
+TEST(QuaternionTest, DefaultConstructor) {
+  const Quaternion q;
+
+  EXPECT_FLOAT_EQ(0.0f, q.x());
+  EXPECT_FLOAT_EQ(0.0f, q.y());
+  EXPECT_FLOAT_EQ(0.0f, q.z());
+  EXPECT_FLOAT_EQ(0.0f, q.w());
+}
+
+TEST(QuaternionTest, CopyConstructor) {
+  const Quaternion q0(1.0f, 2.0f, 3.0f, 4.0f);
+  const Quaternion q1(q0);
+
+  EXPECT_FLOAT_EQ(q0.x(), q1.x());
+  EXPECT_FLOAT_EQ(q0.y(), q1.y());
+  EXPECT_FLOAT_EQ(q0.z(), q1.z());
+  EXPECT_FLOAT_EQ(q0.w(), q1.w());
+}
+
+TEST(QuaternionTest, ComponentInitializationConstructor) {
+  const float x = 1.0f;
+  const float y = 2.0f;
+  const float z = 3.0f;
+  const float w = 4.0f;
+  const Vector3 v(x, y, z);
+  const Quaternion q(v, w);
+
+  EXPECT_FLOAT_EQ(x, q.x());
+  EXPECT_FLOAT_EQ(y, q.y());
+  EXPECT_FLOAT_EQ(z, q.z());
+  EXPECT_FLOAT_EQ(w, q.w());
+}
+
+TEST(QuaternionTest, ArrayInitializationConstructor) {
+  const float a[] = {1.0f, 2.0f, 3.0f, 4.0f};
+  const Quaternion q(a);
+
+  EXPECT_FLOAT_EQ(a[0], q.x());
+  EXPECT_FLOAT_EQ(a[1], q.y());
+  EXPECT_FLOAT_EQ(a[2], q.z());
+  EXPECT_FLOAT_EQ(a[3], q.w());
+}
+
+TEST(QuaternionTest, InitializationConstructor) {
+  const float x = 1.0f;
+  const float y = 2.0f;
+  const float z = 3.0f;
+  const float w = 4.0f;
+  const Quaternion q(x, y, z, w);
+
+  EXPECT_FLOAT_EQ(x, q.x());
+  EXPECT_FLOAT_EQ(y, q.y());
+  EXPECT_FLOAT_EQ(z, q.z());
+  EXPECT_FLOAT_EQ(w, q.w());
+}
+
+TEST(QuaternionTest, GetX) {
+  const float x = 1.0f;
+  const Quaternion q(x, 99.99f, 99.99f, 99.99f);
+
+  EXPECT_FLOAT_EQ(x, q.x());
+}
+
+TEST(QuaternionTest, GetY) {
+  const float y = 1.0f;
+  const Quaternion q(99.99f, y, 99.99f, 99.99f);
+
+  EXPECT_FLOAT_EQ(y, q.y());
+}
+
+TEST(QuaternionTest, GetZ) {
+  const float z = 1.0f;
+  const Quaternion q(99.99f, 99.99f, z, 99.99f);
+
+  EXPECT_FLOAT_EQ(z, q.z());
+}
+
+TEST(QuaternionTest, GetW) {
+  const float w = 1.0f;
+  const Quaternion q(99.99f, 99.99f, 99.99f, w);
+
+  EXPECT_FLOAT_EQ(w, q.w());
+}
+
+TEST(QuaternionTest, GetScalar) {
+  const float s = 1.0f;
+  const Quaternion q(99.99f, 99.99f, 99.99f, s);
+
+  EXPECT_FLOAT_EQ(s, q.scalar());
+}
+
+TEST(QuaternionTest, SetScalar) {
+  const float s = 1.0f;
+  Quaternion q;
+  q.scalar(s);
+
+  EXPECT_FLOAT_EQ(s, q.scalar());
+}
