2e96b20c70f6616ee8077db4322ab7fce9563ff0
Jacob McGladdery <jacobm117@gmail.com>
2013-11-08 21:15:15 -0500

    Tweaked includes order

--- test/kalman_unittest.cpp
+++ test/kalman_unittest.cpp
@@ -21,5 +21,5 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 *******************************************************************************/
 
-#include "../kalman.h"
 #include <gtest/gtest.h>
+#include "../kalman.h"
