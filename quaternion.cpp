2bc1d73a3eca028d3b22cc25de2ba68ec039c226
Jacob McGladdery <jacobm117@gmail.com>
2014-07-26 11:29:30 -0400

    Moved the Quaternion class
    Got rid of the utility folder and moved the Quaternion class to the top
    level directory in this Arduino library. Also fixed a Find-Replace error
    where the include statement for quaternion.h got changed to q.h.

--- quaternion.cpp
+++ quaternion.cpp
@@ -23,7 +23,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <math.h>
 
-#include "q.h"
+#include "quaternion.h"
 
 Quaternion::Quaternion() :
     scalar(1.0f),
